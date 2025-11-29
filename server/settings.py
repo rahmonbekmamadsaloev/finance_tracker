0dac99b4d11583206393bebe89e1d287fa4e3940
Rezvon1409 <rezvon0880@gmail.com>
2025-11-29 17:17:09 +0500

    create CustomUser

--- server/settings.py
+++ server/settings.py
@@ -112,6 +112,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "account.CustomUser"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
