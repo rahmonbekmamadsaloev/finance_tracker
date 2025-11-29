74a7cc1c797c9fdd3b91cb5b5af67525d74f716b
H4CKER <aqdodovirshod7@gmail.com>
2025-11-29 17:27:20 +0500

    Corrected mistake

--- finans_treker/models.py
+++ finans_treker/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from accounts.models import CustomUser
+from account.models import CustomUser
 class Transaction(models.Model):
     TRANSACTION_CHOICES = [
         ('INCOME', 'income'),
